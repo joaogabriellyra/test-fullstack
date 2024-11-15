ceffa0ba96098e4d0aae2e83d0ea9c66b0d930de
joaogabriellyra <jgabriellyra@hotmail.com>
2024-11-15 02:58:26 -0300

    docker compose configurado e drizzle orm instalado

--- backend/src/http/server.ts
+++ backend/src/http/server.ts
@@ -1,7 +1,9 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.listen({
-  port: 3333
-}).then(() => console.log('server listening on port 3333'))
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('server listening on port 3333'))
